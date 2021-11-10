22b46e87ca5248622acf7ec6020014c1a61992bf
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-11-09 21:51:42 -0300

    Test vercel

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,6 +1,5 @@
 import { styles } from '../../styles/about-us/about-us';
 import { OurEnterprise } from '../../components/about-us/OurEnterprise';
-import { Dialogs } from '../../components/about-us/Dialogs';
 import { BoyAndChecks } from '../../components/about-us/BoyAndChecks';
 import { Avatar } from '../../components/about-us/Avatar';
 
@@ -10,8 +9,6 @@ const aboutUs = () => {
     <>
       <div className="about-us-container">
         <OurEnterprise />
-        <Dialogs side={'right'}/>
-        <Dialogs side={'left'}/>
         <BoyAndChecks />
         <Avatar />
       </div>
