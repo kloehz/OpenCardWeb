1ea393c380dbb6cae03efacd014c93d02bd1f1d8
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-11-14 12:31:21 -0300

    [FIX]Fix de importacion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '../components/layout/Layout';
-import { CssBaseline } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <CssBaseline />
       <Component {...pageProps} />
     </Layout>
   );
