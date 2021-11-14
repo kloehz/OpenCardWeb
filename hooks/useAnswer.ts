aadc06d45441d76166452b033da584d61d7412db
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-11-14 12:29:23 -0300

    [ADD]Creacion de nuevo modal

--- hooks/useAnswer.ts
+++ hooks/useAnswer.ts
@@ -1,20 +1,36 @@
+import { IAnswer } from '../types/answer';
 
-export const useAnswer = ( id: number ): string => {
+export const useAnswer = ( id: number ): IAnswer => {
 
-  let answer = 'Sin respuesta';
+  let answer: IAnswer = {
+    title: 'Titulo',
+    content: 'Contenido'
+  };
 
   switch (id) {
   case 0:
-    answer = 'Respuesta numero 0';
+    answer = {
+      title: 'Titulo 1',
+      content: 'Contenido 1'
+    };
     break;
   case 1:
-    answer = 'Respuesta numero 1';
+    answer = {
+      title: 'Titulo 2',
+      content: 'Contenido 2'
+    };
     break;
   case 2:
-    answer = 'Respuesta numero 2';
+    answer = {
+      title: 'Titulo 3',
+      content: 'Contenido 3'
+    };
     break;
   case 3:
-    answer = 'Respuesta numero 3';
+    answer = {
+      title: 'Titulo 4',
+      content: 'Contenido 4'
+    };
     break;
   }
   return answer;
